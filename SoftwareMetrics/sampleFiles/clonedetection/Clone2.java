0937064f71d39ba0e4ac54db0e29f77760c1f6f7
Metchu <ahmednassir@hotmail.com>
2016-11-19 18:30:24 +0100

    Clone test file

---
+++ SoftwareMetrics/sampleFiles/clonedetection/Clone2.java
@@ -0,0 +1,20 @@
+int getDataType(){
+    return dataType;
+}
+int getDisplaySize(){
+	return SSResultSetMetaData.getDisplaySize( dataType, precision, scale);
+}
+void setScale(int scale){
+    this.scale = scale;
+}
+// This is a comment
+// More comment
+int getDataType(){
+    return dataType;
+}
+int getDisplaySize(){
+	return SSResultSetMetaData.getDisplaySize( dataType, precision, scale);
+}
+void setScale(int scale){
+    this.scale = scale;
+}
