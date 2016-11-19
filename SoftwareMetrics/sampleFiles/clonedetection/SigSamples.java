99fd67fbdcae2cf4f07838fe9c00a51e6c4366c5
BaukeBerg <bauke.van.den.berg@gmail.com>
2016-11-19 17:13:16 +0100

    Edited sample for extra whitelines

---
+++ SoftwareMetrics/sampleFiles/clonedetection/SigSamples.java
@@ -0,0 +1,22 @@
+final class SQLParser {
+
+	SSConnection con;
+	private char[] sql;
+    private List tokens;
+    private int tokenIdx;
+
+    Command parse(SSConnection con, String sqlString) throws SQLException{
+    	this.con = con;
+      
+      /** Multi line comment, single lined */
+        Command cmd = parse( sqlString.toCharArray() );
+        SQLToken token = nextToken();
+        if(token != null){
+          // Comment
+          
+        	throw createSyntaxError(token, Language.STXADD_ADDITIONAL_TOK);
+        }
+        return cmd;
+    }	
+	
+}
