7779034624caed9ae73d11203b98697199700777
BaukeBerg <bauke.van.den.berg@gmail.com>
2016-11-24 09:41:21 +0100

    Images

---
+++ SoftwareMetrics/sampleFiles/cyclomaticcomplexity/TernaryOperator.java
@@ -0,0 +1,12 @@
+final class SQLParser {
+
+	SSConnection con;
+	private char[] sql;
+    private List tokens;
+    private int tokenIdx;
+
+    Command parse(SSConnection con, String sqlString) throws SQLException{
+    	true ? false : true;
+    }	
+	
+}
